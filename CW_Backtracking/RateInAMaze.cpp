389c97957c2ef4b2b3ad23bab056e9f26e79b537
Nitesh Sharma <niteshsh34@gmail.com>
2025-08-21 19:22:41 +0530

    DP Method Rate in Maze

--- CW_Backtracking/RateInAMaze.cpp
+++ CW_Backtracking/RateInAMaze.cpp
@@ -1,24 +1,58 @@
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// vector<vector<int>> arr;
+// int n;
+// int cnt = 0;
+
+// void solve(int i, int j) {
+//     if (i < 0 || j < 0 || i >= n || j >= n || arr[i][j] == 0)
+//         return;
+//     if (i == n - 1 && j == n - 1) {
+//         cnt++;
+//         return;
+//     }
+//     arr[i][j] = 0;
+//     solve(i + 1, j);
+//     solve(i - 1, j);
+//     solve(i, j + 1);
+//     solve(i, j - 1);
+//     arr[i][j] = 1;
+// }
+// int main() {
+//     cout << "Enter size of maze (n x n): ";
+//     cin >> n;
+//     arr.resize(n, vector<int>(n));
+//     cout << "Enter maze (1 for open, 0 for blocked):\n";
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < n; j++) {
+//             cin >> arr[i][j];
+//         }
+//     }
+//     solve(0, 0);
+//     cout << "Number of paths = " << cnt << endl;
+//     return 0;
+// }
 #include <iostream>
 #include <vector>
 using namespace std;
 
 vector<vector<int>> arr;
 int n;
-int cnt = 0;
 
-void solve(int i, int j) {
-    if (i < 0 || j < 0 || i >= n || j >= n || arr[i][j] == 0)
-        return;
-    if (i == n - 1 && j == n - 1) {
-        cnt++;
-        return;
+int countPaths(vector<vector<int>>& arr, int n){
+    vector<vector<int>> dp(n, vector<int>(n,0));
+    if(arr[0][0]==0) return 0;
+    dp[0][0]=1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(arr[i][j]==0) continue;
+            if(i>0) dp[i][j]+=dp[i-1][j];
+            if(j>0) dp[i][j]+=dp[i][j-1];
+        }
     }
-    arr[i][j] = 0;
-    solve(i + 1, j);
-    solve(i - 1, j);
-    solve(i, j + 1);
-    solve(i, j - 1);
-    arr[i][j] = 1;
+    return dp[n-1][n-1];
 }
 
 int main() {
@@ -31,7 +65,7 @@ int main() {
             cin >> arr[i][j];
         }
     }
-    solve(0, 0);
-    cout << "Number of paths = " << cnt << endl;
+    int ans = countPaths(arr, n);
+    cout << "Number of paths = " << ans << endl;
     return 0;
 }
