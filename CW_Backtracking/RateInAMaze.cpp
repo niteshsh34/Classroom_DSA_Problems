edf950f9e5bd87d8faf8891d2449fe4de97f7a8a
Nitesh Sharma <niteshsh34@gmail.com>
2025-08-21 18:50:55 +0530

    Create RateInAMaze.cpp

---
+++ CW_Backtracking/RateInAMaze.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<vector<int>>arr;
+int solve(){
+    
+}
+void rateInMaxe(){
+
+}
+int main(){
+    cout<<"Enter number of rows and columns:- ";
+    int n;
+    cin>>n;
+    arr.resize(n);
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+}
